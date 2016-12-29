a28ba2b3df5f38e9e46026b9a1c67838f720c592
ChinaTang <tangdi0802@foxmail.com>
2016-12-29 18:03:19 +0800

    netty包引入

--- src/fristday/LearnFirst.java
+++ src/fristday/LearnFirst.java
@@ -1,10 +1,14 @@
 package fristday;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import jdk.nashorn.internal.runtime.linker.Bootstrap;
 
 /**
  * Created by tangdi on 2016/12/29.
  */
 public class LearnFirst {
-
+    public static void main(String[] args){
+        EventLoopGroup group = new NioEventLoopGroup();
+    }
 }
