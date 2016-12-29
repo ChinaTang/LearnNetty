ce1bf973e8a257ed8f28239d8f8de11bb21c5e45
ChinaTang <tangdi0802@foxmail.com>
2016-12-29 18:46:05 +0800

    第一次netty成功

---
+++ src/fristday/EchoClientHandler.java
@@ -0,0 +1,29 @@
+package fristday;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by tangdi on 2016/12/29.
+ */
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client received: " + ByteBufUtil.hexDump(byteBuf.readBytes(byteBuf.readableBytes())));
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception{
+        ctx.write(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception{
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- src/fristday/LearnFirst.java
+++ src/fristday/LearnFirst.java
@@ -1,14 +1,54 @@
 package fristday;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import jdk.nashorn.internal.runtime.linker.Bootstrap;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
 
 /**
  * Created by tangdi on 2016/12/29.
  */
 public class LearnFirst {
-    public static void main(String[] args){
+
+    private final String host;
+
+    private final int post;
+
+    public LearnFirst(String host, int post){
+        this.host = host;
+        this.post = post;
+    }
+
+    public void start() throws InterruptedException {
         EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).remoteAddress(new InetSocketAddress(host, post))
+                    .handler(new ChannelInitializer<SocketChannel>(){
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = bootstrap.connect().sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+    public static void main(String[] args){
+        try {
+            new LearnFirst("192.168.30.74", 40004).start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
